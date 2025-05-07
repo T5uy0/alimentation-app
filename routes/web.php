bd70fe44a4bf1be81a8ff4ae302cbf7d77f8b688
T5uy0 <julienschneider964@gmail.com>
2025-05-07 11:55:44 +0200

    fix: tailwind

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MealController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::resource('meals', MealController::class);
